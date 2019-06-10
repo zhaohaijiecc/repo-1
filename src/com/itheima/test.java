bd952db7f1785c9e1469246ad3261d0bf0376023
jiecc <823163923@qq.com>
2019-06-10 17:05:10 +0800

    将本地工程提交到本地仓库

--- src/com/itheima/test.java
+++ src/com/itheima/test.java
@@ -4,4 +4,8 @@ public class test {
     public static void main(String[] args) {
         System.out.println("dsdsdsds");
     }
+    public static void test(){
+        System.out.println("我修改了源代码");
+    }
+
 }
